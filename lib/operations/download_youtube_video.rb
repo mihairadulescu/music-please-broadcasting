402c13b31b1ba904b5cad1963fda2bd0df61c485
mihai.radulescu <mihai.radulescu@nerdscomputing.com>
2017-04-20 01:47:55 +0300

    updated views

--- lib/operations/download_youtube_video.rb
+++ lib/operations/download_youtube_video.rb
@@ -4,8 +4,6 @@ module Operations
   class DownloadYoutubeVideo
     def call(input)
       options = {
-          username: 'mihairadulescu89@gmail.com',
-          password: 'stanpapusa',
           rate_limit: '50K',
           format: :worst,
           continue: false,
