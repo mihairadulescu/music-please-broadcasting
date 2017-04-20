58d55bc6f1f3b409f24dcd59ba5cb55beafbd9d3
mihai.radulescu <mihai.radulescu@nerdscomputing.com>
2017-04-20 03:09:47 +0300

    add subscription prewview

--- web/routes/example.rb
+++ web/routes/example.rb
@@ -7,6 +7,21 @@ class MusicPleaseBroadcasting::Application
         test = r.params
         test['hub.challenge']
       end
+
+      r.post do
+        out_file = File.new("out.txt", "w")
+        out_file.puts r.params.to_s
+        out_file.close
+        ''
+      end
+    end
+  end
+
+  route "pagehook-preview" do
+    r.is do
+      r.get do
+        File.read("out.txt")
+      end
     end
   end
 end
